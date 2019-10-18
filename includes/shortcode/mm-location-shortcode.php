120f383a0083ea5b093cd46983f7a7001e9e6dc5
Bernhard Mehler <bernhard.mehler@gmail.com>
2019-10-18 11:03:01 +0200

    Post type wird mit allen Feldern im Theme angezeigt - funktioniert

--- includes/shortcode/mm-location-shortcode.php
+++ includes/shortcode/mm-location-shortcode.php
@@ -16,9 +16,23 @@ function locationShortcode() {
 
 	if ( $query->have_posts() ) :
 		while ( $query->have_posts() ) : $query->the_post(); 
-			$country = get_post_meta( $post->ID, 'country', true );
+			$country 	= get_post_meta( $post->ID, 'country', true );
+			$company 	= get_post_meta( $post->ID, 'company', true );
+			$phone 		= get_post_meta( $post->ID, 'phone', true );
+			$street		= get_post_meta( $post->ID, 'street', true );
+			$city 		= get_post_meta( $post->ID, 'city', true );
+			$country 	= get_post_meta( $post->ID, 'country', true );
+			$email 		= get_post_meta( $post->ID, 'email', true );
+			$maps 		= get_post_meta( $post->ID, 'maps', true );
 			?>
-			<p>Country: <?php echo esc_attr($country) ?>
+			<div>Country: <?php echo esc_attr($country) ?></div>
+			<div>Company: <?php echo esc_attr($company) ?></div>
+			<div>Phone: <?php echo esc_attr($phone) ?></div>
+			<div>Street: <?php echo esc_attr($street) ?></div>
+			<div>City: <?php echo esc_attr($city) ?></div>
+			<div>Country: <?php echo esc_attr($country) ?></div>
+			<div><a href=mailto:<?php echo esc_attr($email) ?>><i class="fa fa-envelope" aria-hidden="true"></i> E-Mail senden</a></div>
+			<div><a href=<?php echo esc_attr($maps) ?>><i class="fa fa-globe" aria-hidden="true"></i> Route berechnen</a></div>
 		<?php
 		endwhile;
 	endif;
