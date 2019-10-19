fd82d24d84355dd6b775fb413f626eb3fbb3802c
Bernhard Mehler <bernhard.mehler@gmail.com>
2019-10-19 15:54:46 +0200

    Widget mit Post-ID input field aber noch keine Query

--- includes/widget/mm-location-widget.php
+++ includes/widget/mm-location-widget.php
@@ -29,10 +29,10 @@ class Foo_Widget extends \WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 		echo $args['before_widget'];
-		if ( ! empty( $instance['title'] ) ) {
-			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		if ( ! empty( $instance['p'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['p'] ) . $args['after_title'];
 		}
-		echo esc_html__( 'Hello, World!', 'text_domain' );
+		//echo esc_html__( 'Hello, World!', 'mm-location' );
 		echo $args['after_widget'];
 	}
 
@@ -44,11 +44,11 @@ class Foo_Widget extends \WP_Widget {
 	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+		$p = ! empty( $instance['p'] ) ? $instance['p'] : esc_html__( 'Post-ID', 'mm-location' );
 		?>
 		<p>
-		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label> 
-		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		<label for="<?php echo esc_attr( $this->get_field_id( 'p' ) ); ?>"><?php esc_attr_e( 'Location Post ID', 'mm-location' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'p' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'p' ) ); ?>" type="text" value="<?php echo esc_attr( $p ); ?>">
 		</p>
 		<?php 
 	}
@@ -65,7 +65,7 @@ class Foo_Widget extends \WP_Widget {
 	 */
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+		$instance['p'] = ( ! empty( $new_instance['p'] ) ) ? sanitize_text_field( $new_instance['p'] ) : '';
 
 		return $instance;
 	}
