32b2c0046e7f013b5fa3233f6ce84615643d2afd
Bernhard Mehler <bernhard.mehler@gmail.com>
2019-10-20 16:28:54 +0200

    Select Options und Radio Group fertig - Wird im Widget Template ausgegeben

--- includes/options/mm-location-options.php
+++ includes/options/mm-location-options.php
@@ -19,13 +19,15 @@ function register_mm_location_submenu_page() {
 function register_mm_location_settings() {
     register_setting('mm-location-option-group', 'location_input_field');
     register_setting('mm-location-option-group', 'location_checkbox_field');
+    register_setting('mm-location-option-group', 'location_select_option_field');
+    register_setting('mm-location-option-group', 'location_radio_option_field');
 }
  
 function mm_location_submenu_page_callback() {
     ?>
     <div class="location-wrap">
         <div id="icon-tools" class="icon32"></div>
-        <h2>Location Options</h2>
+        <h2>Options Examples</h2>
         <form method="post" action="options.php">
         <?php settings_fields('mm-location-option-group'); ?>
         <?php do_settings_sections('mm-location-option-group'); ?>
@@ -40,16 +42,29 @@ function mm_location_submenu_page_callback() {
                 <th scope="row">Test Checkbox</th>
                 <td>
                     <ul class="location-checkbox" id="id-location-checkbox" >
-                        <?php
-                            #if(esc_attr(get_option('location_checkbox_field'))) {
-                            #    $checked = "checked='checked'";
-                            #}
-                            # <?php echo $checked 
-                        ?>
                         <li>
                             <input type="checkbox" name="location_checkbox_field" value="1" <?php checked(1, get_option('location_checkbox_field'), true);?> class="location_checkbox_field" />
                         </li>
                     </ul>
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Test Select Box</th>
+                <td>
+                    <select name="location_select_option_field">
+                        <option value="Napoli" <?php selected(get_option('location_select_option_field'), "Napoli"); ?>>Pizza Napoli</option>
+                        <option value="Funghi" <?php selected(get_option('location_select_option_field'), "Funghi"); ?>>Pizza Funghi</option>
+                        <option value="Mare" <?php selected(get_option('location_select_option_field'), "Mare"); ?>>Pizza Mare</option>
+                        <option value="Tonno" <?php selected(get_option('location_select_option_field'), "Tonno"); ?>>Pizza Tonno</option>
+                    </select>
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Test Radio Select</th>
+                <td>
+                    <input type="radio" name="location_radio_option_field" value="male" <?php checked(get_option('location_radio_option_field'), "male"); ?>> Male<br>
+                    <input type="radio" name="location_radio_option_field" value="female" <?php checked(get_option('location_radio_option_field'), "female"); ?>> Female<br>
+                    <input type="radio" name="location_radio_option_field" value="other" <?php checked(get_option('location_radio_option_field'), "other"); ?>> Other<br> 
                 </td>
             </tr>
         </table>
--- includes/widget/templates/mm-location-widget-template.php
+++ includes/widget/templates/mm-location-widget-template.php
@@ -6,4 +6,7 @@
 <div class="widget-text-country">Country: <?php echo esc_attr($country); ?></div>
 <div><a href=mailto:<?php echo esc_attr($email) ?>><i class="fa fa-envelope" aria-hidden="true"></i> E-Mail senden</a></div>
 <div class="widget-text-maps"><a href=<?php echo esc_attr($maps); ?>><i class="fa fa-globe" aria-hidden="true"></i> Route berechnen</a></div>
-<?php echo get_option('location_checkbox_field');
+<div>Input Field: <?php echo get_option('location_input_field'); ?></div>
+<div>Checkbox Field: <?php echo get_option('location_checkbox_field'); ?></div>
+<div>Select Option Field: <?php echo get_option('location_select_option_field'); ?></div>
+<div>Radio Option Field: <?php echo get_option('location_radio_option_field'); ?></div>
