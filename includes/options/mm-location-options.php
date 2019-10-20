cc1a3dc98764ec9d328381efa74cefcba294b64b
Bernhard Mehler <bernhard.mehler@gmail.com>
2019-10-20 15:44:27 +0200

    Settings Checkbox funktioniert

--- includes/options/mm-location-options.php
+++ includes/options/mm-location-options.php
@@ -17,7 +17,8 @@ function register_mm_location_submenu_page() {
 }
 
 function register_mm_location_settings() {
-    register_setting('mm-location-option-input', 'location_input_field');
+    register_setting('mm-location-option-group', 'location_input_field');
+    register_setting('mm-location-option-group', 'location_checkbox_field');
 }
  
 function mm_location_submenu_page_callback() {
@@ -26,12 +27,30 @@ function mm_location_submenu_page_callback() {
         <div id="icon-tools" class="icon32"></div>
         <h2>Location Options</h2>
         <form method="post" action="options.php">
-        <?php settings_fields('mm-location-option-input'); ?>
-        <?php do_settings_sections('mm-location-option-input'); ?>
+        <?php settings_fields('mm-location-option-group'); ?>
+        <?php do_settings_sections('mm-location-option-group'); ?>
         <table class="form-table">
             <tr valign="top">
                 <th scope="row">Test Input Box</th>
-                <td><input type="text" name="location_input_field" value="<?php echo esc_attr(get_option('location_input_field')); ?>"/></td>
+                <td>
+                    <input type="text" name="location_input_field" value="<?php echo esc_attr(get_option('location_input_field')); ?>"/>
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Test Checkbox</th>
+                <td>
+                    <ul class="location-checkbox" id="id-location-checkbox" >
+                        <?php
+                            #if(esc_attr(get_option('location_checkbox_field'))) {
+                            #    $checked = "checked='checked'";
+                            #}
+                            # <?php echo $checked 
+                        ?>
+                        <li>
+                            <input type="checkbox" name="location_checkbox_field" value="1" <?php checked(1, get_option('location_checkbox_field'), true);?> class="location_checkbox_field" />
+                        </li>
+                    </ul>
+                </td>
             </tr>
         </table>
 
