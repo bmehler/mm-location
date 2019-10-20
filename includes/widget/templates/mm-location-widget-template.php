effe0c4cae66544ecce7db795479b66221a5c616
Bernhard Mehler <bernhard.mehler@gmail.com>
2019-10-20 15:53:28 +0200

    Option in Widget ausgeben - funktioniert

--- includes/widget/templates/mm-location-widget-template.php
+++ includes/widget/templates/mm-location-widget-template.php
@@ -6,3 +6,4 @@
 <div class="widget-text-country">Country: <?php echo esc_attr($country); ?></div>
 <div><a href=mailto:<?php echo esc_attr($email) ?>><i class="fa fa-envelope" aria-hidden="true"></i> E-Mail senden</a></div>
 <div class="widget-text-maps"><a href=<?php echo esc_attr($maps); ?>><i class="fa fa-globe" aria-hidden="true"></i> Route berechnen</a></div>
+<?php echo get_option('location_checkbox_field');
